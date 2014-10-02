a9788185465eea8e6d9ea9fcbc20de463dfb313e
Jimmy Schementi <jschementi@gmail.com>
2014-10-01 22:00:40 -0400

    v0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='flickr_photostream_sorter',
-    version='0.1',
+    version='0.1.1',
     packages=['flickr_photostream_sorter'],
     install_requires=[
         "flickrapi==1.4.4"
